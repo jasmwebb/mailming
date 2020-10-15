fa049905423f2c57aab9b96eb6d3c004af1cbcd9
Jasmine Webb <jmwebb.94@gmail.com>
2020-10-15 14:04:42 -0700

    Implement base blog structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
 const { DateTime } = require("luxon");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL d, yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy년 L월 d일");
   });
-
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
+  });  
 };
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/post.njk
+title: Test
+date: 2020-10-15
+tags:
+  - small-miming
+---
+
+Second test post.
